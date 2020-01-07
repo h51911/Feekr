50950dc1c1fcf6b9dd06406c381ebb4812188dce
czhv5 <1321857934@qq.com>
2020-01-07 15:16:21 +0800

    1.7

--- feekr/src/pages/Dujia.js
+++ feekr/src/pages/Dujia.js
@@ -1,9 +1,19 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import Search from '../components/search';
 
-class Dujia extends Component{
-    render(){
+import { Carousel } from 'antd';
+
+import '../css/Dujia.css';
+
+class Dujia extends Component {
+    render() {
         return <div>
-            Dujia
+            <Search />
+            <Carousel>
+                <div className="slick-slide"><img src="https://banner01.feekr.com/2019/12/25/0524295e032acda5c37.jpg!750X360"></img></div>
+                {/* <div><img src="https://banner01.feekr.com/2019/12/25/0524295e032acda5c37.jpg!750X360"></img></div>
+                <div><img src="https://banner01.feekr.com/2019/12/25/0524295e032acda5c37.jpg!750X360"></img></div> */}
+            </Carousel>
         </div>
     }
 }
